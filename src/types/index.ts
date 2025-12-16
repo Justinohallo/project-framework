df0cd6992c09c92b65beda333c1eaa662738632b
Justin O'Halloran <justinohallo@gmail.com>
2025-12-16 11:02:17 -0800

    chore: export User type from @prisma/client

--- src/types/index.ts
+++ src/types/index.ts
@@ -1,5 +1,5 @@
 // Export types from Prisma
-export type { User } from "../generated/prisma";
+export type { User } from "@prisma/client";
 
 // Add custom application types here
 export interface ApiResponse<T = unknown> {
